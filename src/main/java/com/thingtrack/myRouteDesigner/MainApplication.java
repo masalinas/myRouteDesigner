4e95403190cac5101d1183ebaac42c2c35a8b409
Miguel Salinas Gancedo <miguel@thingtrack.com>
2013-10-15 10:24:35 +0200

    update

--- src/main/java/com/thingtrack/myRouteDesigner/MainApplication.java
+++ src/main/java/com/thingtrack/myRouteDesigner/MainApplication.java
@@ -15,7 +15,7 @@ public class MainApplication extends Application
 
     @Override
     public void init()
-    {
+    {		
     	setTheme("myRouteDesigner");
     	
     	Window mainWindow = new Window("MyRouteDesigner Sample Application");
@@ -26,7 +26,14 @@ public class MainApplication extends Application
 	    mainLayout.addComponent(new MainLayout());
 	    
         setMainWindow(mainWindow);
-       
+ 
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("jQuery.ajax({");
+		buffer.append(" url: \"https://thingtrack.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e/en_US-7jch18-1988229788/6144/71/1.4.0-m6/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=dad45031\"");
+		buffer.append(",type: \"get\"");
+		buffer.append(",cache: true");
+		buffer.append(",dataType: \"script\"});");
+		getMainWindow().executeJavaScript(buffer.toString());
     }
 
 }
